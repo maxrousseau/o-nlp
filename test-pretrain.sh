2d5f0b7df50b0301dbb175c33291b39251340241
roum5 <roum5@MAX20PC>
2023-03-25 14:57:21 -0400

    add deps

--- test-pretrain.sh
+++ test-pretrain.sh
@@ -1,3 +1,13 @@
+pip install transformers
+pip install absl-py
+pip install peft
+pip install accelerate
+pip install datasets
+pip install sentencepiece
+pip install wandb
+
+wandb login 7a817528b63f37a967ac341e84ae3ba00dd9f18e
+
 python main.py \
  --name="t5-default-pretrain" \
  --lr=1e-4 \
