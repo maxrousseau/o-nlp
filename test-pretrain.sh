c0c67647a1938e30a0ab2981327b604c611649f5
roum5 <roum5@MAX20PC>
2023-03-25 20:46:18 -0400

    shell issue again grid ai

--- test-pretrain.sh
+++ test-pretrain.sh
@@ -15,7 +15,7 @@ python main.py \
  --lr_scheduler=False \
  --model_checkpoint="google/t5-v1_1-base" \
  --tokenizer_checkpoint="google/t5-v1_1-base" \
- --train_dataset="/datastores/tapp-datastore-2/tgt-beta-0-85t-86k" \
+ --train_dataset="/datastores/tapp-datastore-2/tgt-beta-0.85t-86k" \
  --max_seq_len=512 \
  --max_ans_len=128 \
  --seed=0 \
