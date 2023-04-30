305e38a2cead42d7fd49db181d60926580382f3d
roum5 <maximerousseau08@gmail.com>
2023-04-30 12:00:54 -0400

    biobart first

--- scripts/biomed_seq2seq_exp.sh
+++ scripts/biomed_seq2seq_exp.sh
@@ -8,6 +8,21 @@ pip install wandb
 
 wandb login 7a817528b63f37a967ac341e84ae3ba00dd9f18e
 
+python main.py \
+    --name="biobart-base-sft-oqa" \
+     --lr=2e-5 \
+     --epochs=35 \
+     --lr_scheduler=False \
+     --model_checkpoint="GanjinZero/biobart-base" \
+     --tokenizer_checkpoint="GanjinZero/biobart-base" \
+     --train_dataset="/datastores/oqav1/bin/train" \
+     --val_dataset="/datastores/oqav1/bin/val" \
+     --max_seq_len=1024 \
+     --max_ans_len=256 \
+     --seed=0 \
+     --runmode="bart-finetune" \
+     --savedir="./biobart_ckpts" \
+
 
 python main.py \
      --name="scifive-base-sft-oqa" \
@@ -23,18 +38,3 @@ python main.py \
      --seed=0 \
      --runmode="t5-finetune" \
      --savedir="./scifive_ckpts" \
-
-python main.py \
-    --name="biobart-base-sft-oqa" \
-     --lr=2e-5 \
-     --epochs=35 \
-     --lr_scheduler=False \
-     --model_checkpoint="GanjinZero/biobart-base" \
-     --tokenizer_checkpoint="GanjinZero/biobart-base" \
-     --train_dataset="/datastores/oqav1/bin/train" \
-     --val_dataset="/datastores/oqav1/bin/val" \
-     --max_seq_len=1024 \
-     --max_ans_len=256 \
-     --seed=0 \
-     --runmode="bart-finetune" \
-     --savedir="./biobart_ckpts" \
