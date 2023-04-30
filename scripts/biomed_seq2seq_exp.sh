af7e5336d6dd2c8452afbd80347fd1537498cb40
roum5 <maximerousseau08@gmail.com>
2023-04-30 13:05:42 -0400

    bart sft script

--- scripts/biomed_seq2seq_exp.sh
+++ scripts/biomed_seq2seq_exp.sh
@@ -24,17 +24,17 @@ python main.py \
      --savedir="./biobart_ckpts" \
 
 
-python main.py \
-     --name="scifive-base-sft-oqa" \
-     --lr=1e-4 \
-     --epochs=35 \
-     --lr_scheduler=False \
-     --model_checkpoint="razent/SciFive-base-Pubmed_PMC"\
-     --tokenizer_checkpoint="razent/SciFive-base-Pubmed_PMC" \
-     --train_dataset="/datastores/oqav1/bin/train" \
-     --val_dataset="/datastores/oqav1/bin/val" \
-     --max_seq_len=768 \
-     --max_ans_len=256 \
-     --seed=0 \
-     --runmode="t5-finetune" \
-     --savedir="./scifive_ckpts" \
+# python main.py \
+#     --name="scifive-base-sft-oqa" \
+#     --lr=1e-4 \
+#     --epochs=35 \
+#     --lr_scheduler=False \
+#     --model_checkpoint="razent/SciFive-base-Pubmed_PMC"\
+#     --tokenizer_checkpoint="razent/SciFive-base-Pubmed_PMC" \
+#     --train_dataset="/datastores/oqav1/bin/train" \
+#     --val_dataset="/datastores/oqav1/bin/val" \
+#     --max_seq_len=768 \
+#     --max_ans_len=256 \
+#     --seed=0 \
+#     --runmode="t5-finetune" \
+#     --savedir="./scifive_ckpts" \
