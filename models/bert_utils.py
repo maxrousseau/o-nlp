eab0b5f2d5acbf4a9fcf51ea35f0f35a206d84f7
roum5 <maximerousseau08@gmail.com>
2023-05-22 13:18:35 -0400

    tracking val loss now

--- models/bert_utils.py
+++ models/bert_utils.py
@@ -477,9 +477,9 @@ def setup_metatune(train_path, val_path, config, only_head=False):
     squad = load_dataset(
         "squad",
     )  # @BUG remove for caching
-    config.big_dataset = squad["train"].select(range(16))
-    config.train_dataset = Dataset.load_from_disk(train_path).select(range(16))
-    config.val_dataset = Dataset.load_from_disk(val_path).select(range(16))
+    config.big_dataset = squad["train"]
+    config.train_dataset = Dataset.load_from_disk(train_path)
+    config.val_dataset = Dataset.load_from_disk(val_path)
 
     # !bert
 
