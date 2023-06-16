dd062f51d3d1bc892102202a7be189c53376e1a2
Maxime Rousseau <maximerousseau08@gmail.com>
2023-06-16 10:06:22 -0400

    iter

--- models/llm_inference.py
+++ models/llm_inference.py
@@ -17,6 +17,10 @@ from accelerate import init_empty_weights, Accelerator
 import torch
 from torch.utils.data import DataLoader
 
+import re
+import string
+from collections import Counter
+
 
 # https://huggingface.co/spaces/evaluate-metric/f1 just implement a simple evaluate metric
 """
@@ -159,9 +163,28 @@ Refer to the passage below and answer the following question:\n\nPassage: {conte
 
         return Dataset.from_dict(self.samples)
 
+    def normalize_answer(self, s):
+        """Lower text and remove punctuation, articles and extra whitespace."""
+
+        def remove_articles(text):
+            return re.sub(r"\b(a|an|the)\b", " ", text)
+
+        def white_space_fix(text):
+            return " ".join(text.split())
+
+        def remove_punc(text):
+            exclude = set(string.punctuation)
+            return "".join(ch for ch in text if ch not in exclude)
+
+        def lower(text):
+            return text.lower()
+
+        return white_space_fix(remove_articles(remove_punc(lower(s))))
+
+
     def f1_score(self, prediction, ground_truth):
-        prediction_tokens = normalize_answer(prediction).split()
-        ground_truth_tokens = normalize_answer(ground_truth).split()
+        prediction_tokens = self.__normalize_answer(prediction).split()
+        ground_truth_tokens = self.__normalize_answer(ground_truth).split()
         common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
         num_same = sum(common.values())
         if num_same == 0:
